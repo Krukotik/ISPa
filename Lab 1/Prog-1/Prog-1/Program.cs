4bc5f41c8692cab60a7a19e78042b05518f2baa7
Krukotik <79203490+Krukotik@users.noreply.github.com>
2021-04-02 16:52:13 +0300

    Add files via upload

---
+++ Lab 1/Prog-1/Prog-1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                   
+            char again = 'y';
+            Random rand = new Random();
+
+            while (again == 'y')
+            {
+
+                int i = rand.Next(100);
+
+                Console.WriteLine("Компьютер загадал число от 0 до 100");
+
+                if (i < 50) Console.WriteLine("Число меньше 50");
+                else Console.WriteLine("Число больше или равно 50");
+
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                if (i == x) Console.WriteLine("Вы победили свою пекарню!");
+                else Console.WriteLine("Вы проиграли,пекарня загадала число {0}", i);
+
+                Console.WriteLine("Попробовать еще?(y = Да,n = Нет)");
+                again = Convert.ToChar(Console.ReadLine());
+              
+            }
+
+        }
+    }
+}
+
