67a1a40a7335d40a9129ee9c26e880e697bdcb88
Krukotik <79203490+Krukotik@users.noreply.github.com>
2021-06-04 20:51:17 +0300

    Add files via upload

---
+++ Lab 6/Prog-6/Lab6/Interface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public interface IPlan
+{
+    public void TestCovid();
+
+    public void TimePressure(int counter);
+}
+
